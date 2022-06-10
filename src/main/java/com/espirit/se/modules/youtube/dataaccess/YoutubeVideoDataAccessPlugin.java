0916e78527f041a1146d5b407917beb5fbc33af1
Niels Hauke <hauke@e-spirit.com>
2022-06-10 10:31:13 +0200

    SE-820 added report items executable test

--- src/main/java/com/espirit/se/modules/youtube/dataaccess/YoutubeVideoDataAccessPlugin.java
+++ src/main/java/com/espirit/se/modules/youtube/dataaccess/YoutubeVideoDataAccessPlugin.java
@@ -114,7 +114,7 @@ public class YoutubeVideoDataAccessPlugin implements DataAccessPlugin<YoutubeVid
 		/**
 		 * Basic class to handle report icons, i.e. click event, etc.. Please see FirstSpirit API for more information.
 		 */
-		private class YoutubeVideoPreviewItem implements JavaClientExecutableReportItem<YoutubeVideo>, WebeditExecutableReportItem<YoutubeVideo> {
+		static class YoutubeVideoPreviewItem implements JavaClientExecutableReportItem<YoutubeVideo>, WebeditExecutableReportItem<YoutubeVideo> {
 
 			@Override
 			public boolean isVisible(ReportContext<YoutubeVideo> context) {
